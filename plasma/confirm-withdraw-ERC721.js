a2303adc2d238b7fb2f96ed0ab9280393fb7e947
rahuldamodar94 <rahuldamodar94@gmail.com>
2020-07-29 05:18:40 +0530

    new: predicate withdraw

--- plasma/confirm-withdraw-ERC721.js
+++ plasma/confirm-withdraw-ERC721.js
@@ -1,6 +1,7 @@
 const Matic = require("@maticnetwork/maticjs").default;
 const config = require("./config");
 const from = config.FROM_ADDRESS; // from address
+const predicate = config.ERC721_PREDICATE;
 
 // Create object of Matic
 const matic = new Matic({
@@ -12,12 +13,13 @@ const matic = new Matic({
   registry: config.REGISTRY,
 });
 
-var transactionHash = "transactionHash from initiate withdraw";
+var transactionHash =
+  "0x6d80f2a3c54f221f8df31c4b1bcfd72f46f83ee8a64c904a79b3d08eb29f0987";
 
 matic.initialize().then(() => {
   matic.setWallet(config.PRIVATE_KEY);
   matic
-    .withdrawNFT(transactionHash, {
+    .withdrawMintableERC721Token(transactionHash, predicate, {
       from,
     })
     .then((res) => {
